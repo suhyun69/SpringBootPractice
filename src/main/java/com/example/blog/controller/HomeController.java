6ddb8c0b628c342d9b481c125e2b5bcc69f9d67a
suhpark <suhpark@gmarket.com>
2023-12-21 23:15:02 +0900

    add SpringDoc

--- src/main/java/com/example/blog/controller/HomeController.java
+++ src/main/java/com/example/blog/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.example.blog.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
+@Tag(name = "Template", description = "템플릿 API Document")
 public class HomeController {
 
     @GetMapping("/hello")
+    @Operation(summary = "템플릿 리스트", description = "템플릿의 모든 리스트를 조회합니다.")
     public String test() {
         return "Hello, stranger.";
     }
