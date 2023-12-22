783b420ba9da55a3a112f1155165ce340f31ac7e
suhpark <suhpark@gmarket.com>
2023-12-22 12:56:18 +0900

    수정

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -7,23 +7,22 @@ import com.example.blog.service.BlogService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/blog")
 @Tag(name = "Blog", description = "Blog API Document")
 public class BlogController {
 
     private final BlogService blogService;
 
-    @PostMapping("/api/article")
+    @PostMapping("/article")
     @Operation(summary = "Article 작성", description = "Article을 작성합니다.")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
@@ -32,7 +31,7 @@ public class BlogController {
                 .body(savedArticle);
     }
 
-    @GetMapping("/api/articles")
+    @GetMapping("/articles")
     @Operation(summary = "Article 전체 조회", description = "전체 Article을 조회합니다.")
     public ResponseEntity<List<ArticleResponse>> findAllArticles() {
         List<ArticleResponse> articles = blogService.findAll()
---
+++ src/main/java/com/example/blog/service/impl/BlogServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.blog.service.impl;
+
+import com.example.blog.domain.Article;
+import com.example.blog.dto.AddArticleRequest;
+import com.example.blog.repository.BlogRepository;
+import com.example.blog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service
+public class BlogServiceImpl implements BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+}
