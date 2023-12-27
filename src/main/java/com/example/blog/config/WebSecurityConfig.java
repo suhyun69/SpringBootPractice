d928736d979838b7de923698f2ba68d8f6d04be9
suhpark <suhpark@gmarket.com>
2023-12-27 14:49:48 +0900

    SpringSecurity 의존성 추가

---
+++ src/main/java/com/example/blog/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.blog.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+    // 스프링 시큐리티 기능 비활성화
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                // .requestMatchers("/static/**")
+                .requestMatchers(toH2Console());
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(authorize -> authorize.anyRequest().permitAll()) // 특정 경로에 대한 Access 설정
+                .logout(logout -> logout // 로그아웃 설정
+                        .invalidateHttpSession(true)) // 로그아웃 이후에 세션을 전체 삭제할 지 여부를 설정
+                .csrf(csrf -> csrf.disable()) // CSRF 설정 비활성화. CSRF 공격을 방지하기 위해서는 활성하는 게 좋지만 실습을 편리하게 하기 위해 비활성화한다
+                .build();
+    }
+}
